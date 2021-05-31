9dace4a9995999533c2d8c398ceaa0d16f923592
MatheusBlodow <matheusblodow@gmail.com>
2021-05-30 22:41:37 -0300

    projeto correto

--- src/JogoGourmet.java
+++ src/JogoGourmet.java
@@ -19,6 +19,8 @@ public class JogoGourmet extends JFrame {
 	static String tipoPrato = "";
 	static ArrayList<String> prato = new ArrayList();
 	static ArrayList<String> grupo = new ArrayList();
+	static boolean encerrou = true;
+	static boolean entrouOpcoes = true;
 	
 	void telaPrincipal() {
 		 Container janela = getContentPane();
@@ -49,39 +51,54 @@ public class JogoGourmet extends JFrame {
 	}
 	
 	static void perguntasTeste(){
-		System.out.println(prato.size());
+		
 		if(prato.size()==0) {
 			prato.add("0 lasanha");
 			grupo.add("massa");
 		}
 		
+		entrouOpcoes = true;
+		encerrou = true;
 		for(int j=0; j<grupo.size();j++) {
-			int validorEscolha = JOptionPane.showConfirmDialog(null, "O prato que você pensou foi "+ grupo.get(j) + "?","Confirm", JOptionPane.YES_NO_OPTION);
-			if(validorEscolha == 1 && j == grupo.size()-1) {
-				validorEscolha = JOptionPane.showConfirmDialog(null, "O prato que você pensou foi bolo de chocolate?","Confirm", JOptionPane.YES_NO_OPTION);
-				if(validorEscolha == 0) {
-					JOptionPane.showMessageDialog(null, "Acertei de novo");
-					break;
-				}else{
-					tipoPrato = JOptionPane.showInputDialog(null,"qual prato você pensou?");
-					prato.add(Integer.toString(grupo.size()) + " "+ tipoPrato);
-					tipoGrupo = JOptionPane.showInputDialog(null,"é ________ mas Bolo de Chocolate não");
-					grupo.add(tipoGrupo);
-					break;
-				}	
-			}else if(validorEscolha == 0) {
-				for(int i=0; i<prato.size();i++) {
-					String testePrato = prato.get(i);
-					if(testePrato.substring(0,1).equals(Integer.toString(j))) {
-						validorEscolha = JOptionPane.showConfirmDialog(null, "O prato que você pensou foi "+ prato.get(i).substring(2)+ "?","Confirm", JOptionPane.YES_NO_OPTION);
-						if(validorEscolha == 1 && i == prato.size()-1 ) {
-							tipoPrato = JOptionPane.showInputDialog(null,"qual prato você pensou?");
-							prato.add(Integer.toString(grupo.size()-1) + " "+ tipoPrato);
-							break;
-						}else if(validorEscolha == 0) {
-							JOptionPane.showMessageDialog(null, "Acertei de novo");
-							break;
+			if(encerrou == true) {
+				int validorEscolha = JOptionPane.showConfirmDialog(null, "O prato que você pensou foi "+ grupo.get(j) + "?","Confirm", JOptionPane.YES_NO_OPTION);
+				if(validorEscolha == 1 && j == grupo.size()-1) {
+					validorEscolha = JOptionPane.showConfirmDialog(null, "O prato que você pensou foi bolo de chocolate?","Confirm", JOptionPane.YES_NO_OPTION);
+					if(validorEscolha == 0) {
+						JOptionPane.showMessageDialog(null, "Acertei de novo","Jogo Gourmet",1);
+						encerrou = false;
+						break;
+					}else{
+						tipoPrato = JOptionPane.showInputDialog(null,"qual prato você pensou?","Desisto",3);
+						prato.add(Integer.toString(grupo.size()) + " "+ tipoPrato);
+						tipoGrupo = JOptionPane.showInputDialog(null,"é ________ mas Bolo de Chocolate não","Complete",3);
+						grupo.add(tipoGrupo);
+						break;
+					}	
+				}else if(validorEscolha == 0) {
+					for(int i=0; i<prato.size();i++) {
+						String testePrato = prato.get(i);
+						if(testePrato.substring(0,1).equals(Integer.toString(j))) {
+							validorEscolha = JOptionPane.showConfirmDialog(null, "O prato que você pensou foi "+ prato.get(i).substring(2)+ "?","Confirm", JOptionPane.YES_NO_OPTION);
+							int testarPrato = Integer.parseInt(testePrato.substring(0,1));
+							if(validorEscolha == 1 && i == prato.size()-1) {
+								tipoPrato = JOptionPane.showInputDialog(null,"qual prato você pensou?","Desisto",3);
+								prato.add(Integer.toString(grupo.size()-1) + " "+ tipoPrato);
+								entrouOpcoes = false;
+								break;
+							}else if(validorEscolha == 0) {
+								JOptionPane.showMessageDialog(null, "Acertei de novo", "Jogo Gourmet",1);
+								encerrou = false;
+								entrouOpcoes = false;
+								break;
+							}
 						}
+						encerrou = false;
+					}
+					if(entrouOpcoes==true) {
+						tipoPrato = JOptionPane.showInputDialog(null,"qual prato você pensou?","Desisto",3);
+						prato.add(Integer.toString(j) + " "+ tipoPrato);
+						break;
 					}
 				}
 			}
